f24721807cea6e457a38dfd0b6b68a1689a15789
David Watkins <djrival7@gmail.com>
2017-10-05 19:38:07 -0400

    Changed VTK5 dependency to VTK6

--- objrec_ros_integration/src/vtk_to_stl.cpp
+++ objrec_ros_integration/src/vtk_to_stl.cpp
@@ -22,7 +22,11 @@ int main(int argc, char *argv[])
 
   vtkSmartPointer<vtkDecimatePro> decimate =
     vtkSmartPointer<vtkDecimatePro>::New();
+#if VTK_MAJOR_VERSION <= 5
   decimate->SetInput(input);
+#else
+  decimate->SetInputData(input);
+#endif
   //decimate->SetTargetReduction(.99); //99% reduction (if there was 100 triangles, now there will be 1)
   decimate->SetTargetReduction(.80); //10% reduction (if there was 100 triangles, now there will be 90)
   decimate->Update();
@@ -31,13 +35,13 @@ int main(int argc, char *argv[])
     vtkSmartPointer<vtkPolyData>::New();
   decimated->ShallowCopy(decimate->GetOutput());
 
-
-
-
- 
   vtkSmartPointer<vtkSTLWriter> writer = vtkSmartPointer<vtkSTLWriter>::New();
   writer->SetFileName(outputFileName.c_str());
+#if VTK_MAJOR_VERSION <= 5
   writer->SetInput(decimated);
+#else
+  writer->SetInputData(decimated);
+#endif  
   writer->SetFileTypeToBinary();
   writer->Update();
  
